ddd07bd8b8a86147f5537a0bf3091e7a89e4cc87
Kristen Wishart <kwishart24@gmail.com>
2026-07-02 21:40:02 -0400

    created header, about, and footer components

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hi, my name is Kristen and I'm a full-stack developer! I've been coding
+        for almost 6 years on and off now. My first exposure to coding was
+        during COVID in 2020, taking a couple free coding webinars through
+        General Assembly from my Boston apartment as the world was shut down.
+        Ever since then, I've had other dreams I wanted to pursue, but I always
+        came back to coding. Looking through all of my projects, I'm so proud of
+        how far I've come as a software developer and can't wait to see what I
+        can create next!
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Kristen Wishart</h1>
+      <p>Full-Stack Developer</p>
+    </header>
+  );
+}
+
+export default Header;
