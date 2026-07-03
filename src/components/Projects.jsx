39a0183c417accff031d2c8dbe79dcb578c1adc8
Kristen Wishart <kwishart24@gmail.com>
2026-07-03 00:08:05 -0400

    added all project photos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import ProjectCard from "./ProjectCard";
 import TaskTracker from "../assets/task-tracker-api.png";
+import NodeHw from "../assets/node-homework-api.png";
+import Rolodex from "../assets/rolodex.png";
 
 function Projects() {
   return (
@@ -15,13 +17,15 @@ function Projects() {
         />
         <ProjectCard
           title="Node Homework"
-          projectImage={TaskTracker}
-          altText="Postman testing of Task Tracker API shows successful user registration."
+          projectImage={NodeHw}
+          altText="Swagger documentation for the Node Homework API."
           description="This TaskList App is a secure, production-grade backend built with Node.js, Express, Prisma, and PostgreSQL. The API powers a task management system where authenticated users can create, update, search, and analyze their tasks. Its purpose is to demonstrate real-world backend engineering skills."
           technologies="Node.js, Express, PostgreSQL (Neon.tech), Prisma, JWT (HTTP-only cookies), Joi, Helmet, rate limiting, sanitization, Jest, Supertest, Render, Swagger"
         />
         <ProjectCard
           title="Rolodex"
+          projectImage={Rolodex}
+          altText="Swagger documentation for the Node Homework API."
           description="Rolodex is a full-stack React application for organizing professional contacts and keeping track of important interactions. It combines a clean, intuitive UI with a real Airtable backend, allowing users to create, update, and manage contacts, upload headshots, and attach notes — all in one place."
           technologies="React + React Router, Airtable API, Cloudinary Upload, Vite"
         />
